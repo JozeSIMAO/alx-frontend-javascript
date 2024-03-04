9993dd6c1e4d1ebdf3a35be5e9d9ffbaf25d4d0f
JozeSIMAO <jozesimao10@gmail.com>
2024-03-04 23:07:12 +0200

    Create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    allEmployees: employeesList,
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
     },
   };
 }
