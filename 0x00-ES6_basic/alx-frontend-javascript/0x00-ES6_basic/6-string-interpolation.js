c8e8ae8911cea7305aaa61d1d6caeb5249396b93
JozeSIMAO <jozesimao10@gmail.com>
2024-03-04 15:11:09 +0200

    Arrow functions

---
+++ 0x00-ES6_basic/alx-frontend-javascript/0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,10 @@
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '19,868',
+    gdp: '54.2 billion',
+    capita: '78,479',
+  };
+
+  return ;
+}
