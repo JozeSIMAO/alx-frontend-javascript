bea582752a7eaa7b4c75d1912c4ed8519a7c20cf
JozeSIMAO <jozesimao10@gmail.com>
2024-03-05 23:52:32 +0200

    updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulating an asynchronous API call
-        setTimeout(() => {
-            // Assuming the API call is successful, resolve the promise
-            resolve("Response from API");
-        }, 1000); // Simulating a delay of 1 second
-    });
+export default function getResponseFromAPI() {
+    return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
