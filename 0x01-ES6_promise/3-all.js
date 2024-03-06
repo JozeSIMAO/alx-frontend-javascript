4896d9054150b2e6e429db466d6656b58ece16d7
JozeSIMAO <jozesimao10@gmail.com>
2024-03-06 13:51:52 +0200

    eslint fixs

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-        .then(([photoData, userData]) => {
-            console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
-        })
-        .catch(() => {
-            console.log("Signup system offline");
-        });
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 export default handleProfileSignup;
