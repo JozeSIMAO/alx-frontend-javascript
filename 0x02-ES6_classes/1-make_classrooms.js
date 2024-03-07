6f9719c3f7df880a8d76a5e9f75dbfdf4f9e9eac
JozeSIMAO <jozesimao10@gmail.com>
2024-03-07 23:24:44 +0200

    lets make some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import Classroom from './0-classroom';
+import classRoom from './0-classroom';
+
+export default function initializeRooms() {
+    return [
+        new Classroom(19),
+        new Classroom(20),
+        new Classroom(34)
+    ];
+}
