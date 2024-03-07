0f94ef2a082f9cb16941b688ad062d2e1c9dd63c
JozeSIMAO <jozesimao10@gmail.com>
2024-03-07 22:31:33 +0200

    0-classromm.js

--- 0x02-ES6_classes/babel.config.js
+++ 0x02-ES6_classes/babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      targets: {
-        node: '12.11',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
       },
-    }],
+    ],
   ],
 };
