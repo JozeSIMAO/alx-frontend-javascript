605d3af03d8e45c4d77b331bd3438759f03667df
JozeSIMAO <jozesimao10@gmail.com>
2024-03-08 13:31:59 +0200

    A course, Getters, and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,5 @@
 export default class HolbertonCourse{
     constructor(name, length, students) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        if (typeof length !== 'number') {
-            throw new TypeError('Length must be a number');
-        }
         this._name = name;
         this._length = length;
         this._students = students;
@@ -38,6 +32,12 @@ export default class HolbertonCourse{
     }
 
     set students(newStudents) {
+        if (!(value instanceof Array)) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        if (!value.every((student) => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
         this._students = newStudents;
     }
 }
